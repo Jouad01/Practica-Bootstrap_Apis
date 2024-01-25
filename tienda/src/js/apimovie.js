c3de77d641f38f971be3659817650a2b310b5403
Jouad <jouadelouardi64@gmail.com>
2024-01-25 16:46:25 +0100

    feat: agregado notas y la opcion de trailer al modal desde la api

--- tienda/src/js/apimovie.js
+++ tienda/src/js/apimovie.js
@@ -7,27 +7,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
 export const getPopularMovies = async (page) => {
   const moviesUrl = `${URL_PATH}/3/movie/popular?api_key=${API_KEY}&language=es-ES&page=${page}`;
-  const genresUrl = `${URL_PATH}/3/genre/movie/list?api_key=${API_KEY}&language=es-ES`;
-  // const ratedUrl = `${URL_PATH}/3/movie/top_rated?api_key=${API_KEY}&language=es-ES&page=${page}`;
 
-  const [moviesResponse, genresResponse] = await Promise.all([
-    fetch(moviesUrl).then(response => response.json()),
-    fetch(genresUrl).then(response => response.json())
+  const [moviesResponse] = await Promise.all([
+    fetch(moviesUrl).then(response => response.json())
   ]);
 
-  // Map genres to movies based on genre id
-  const genresMap = {};
-  genresResponse.genres.forEach(genre => {
-    genresMap[genre.id] = genre.name;
+  // Combine movie data with genres
+  const moviesWithGenres = moviesResponse.results.map(async movie => {
+    const genresUrl = `${URL_PATH}/3/movie/${movie.id}?api_key=${API_KEY}&language=es-ES`;
+    const videosUrl = `${URL_PATH}/3/movie/${movie.id}/videos?api_key=${API_KEY}&language=en-US`;
+
+    const [genresResponse, videosResponse] = await Promise.all([
+      fetch(genresUrl).then(response => response.json()),
+      fetch(videosUrl).then(response => response.json())
+    ]);
+
+    const genresMap = {};
+    genresResponse.genres.forEach(genre => {
+      genresMap[genre.id] = genre.name;
+    });
+
+    return {
+      ...movie,
+      genres: movie.genre_ids.map(genreId => genresMap[genreId] || 'NOT FOUND'),
+      videos: videosResponse.results
+    };
   });
 
-  // Combine movie data with genres
-  const moviesWithGenres = moviesResponse.results.map(movie => ({
-    ...movie,
-    genres: movie.genre_ids.map(genreId => genresMap[genreId] || 'NOT FOUND')
-  }));
-
-  return moviesWithGenres;
+  return Promise.all(moviesWithGenres);
 };
 
 
@@ -35,45 +42,47 @@ export const renderPopularMovies = async (page) => {
   const movies = await getPopularMovies(page);
 
   let html = "";
-  movies.slice(0, 8).forEach((movie, index) => {
-    const { id, title, poster_path, overview, release_date, genres } = movie;
+  for (let index = 0; index < 8; index++) {
+    const movie = movies[index];
+    const { id, title, poster_path, overview, release_date, genres, vote_average, videos } = movie;
     const urlImage = `https://image.tmdb.org/t/p/w500${poster_path}`;
     const urlMoreInfo = `../pelis.html?id=${id}`;
+    const trailerLink = videos.length > 0 ? `https://www.youtube.com/watch?v=${videos[0].key}` : '#';
 
     html += `
-        <div class="col-md-3 col-sm-6 col-12 mb-4">
-          <div class="card h-100">
-            <img src="${urlImage}" class="card-img-top img-fluid" style="object-fit: cover;" alt="${title}">
-            <div class="card-body d-flex flex-column">
-              <h5 class="card-title text-center m-0">${title}</h5>
-              <button type="button" class="btn btn-primary mt-auto" data-bs-toggle="modal" data-bs-target="#movieModal${index}">
-                Ver detalles
-              </button>
-            </div>
+      <div class="col-md-3 col-sm-6 col-12 mb-4">
+        <div class="card h-100">
+          <img src="${urlImage}" class="card-img-top img-fluid" style="object-fit: cover;" alt="${title}">
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title text-center m-0">${title}</h5>
+            <button type="button" class="btn btn-primary mt-auto" data-bs-toggle="modal" data-bs-target="#movieModal${index}">
+              Ver detalles
+            </button>
           </div>
-          <!-- Modal con los detalles de la película -->
-          <div class="modal fade" id="movieModal${index}" tabindex="-1" aria-labelledby="movieModalLabel${index}" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="movieModalLabel${index}">${title}</h5>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                  <p>${overview}</p>
-                  <p>Fecha de lanzamiento: ${release_date}</p>
-                  <p>Géneros: ${genres.join(', ')}</p>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                  <a href="${urlMoreInfo}" target="_blank" class="btn btn-primary">Ver tráiler</a>
-                </div>
+        </div>
+        <!-- Modal con los detalles de la película -->
+        <div class="modal fade" id="movieModal${index}" tabindex="-1" aria-labelledby="movieModalLabel${index}" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="movieModalLabel${index}">${title}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <p>${overview}</p>
+                <p>Fecha de lanzamiento: ${release_date}</p>
+                <p>Géneros: ${genres.join(', ')}</p>
+                <p>Nota: ${vote_average.toFixed(1)}</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                <a href="${trailerLink}" target="_blank" class="btn btn-primary">Ver más</a>
               </div>
             </div>
           </div>
         </div>
-      `;
-  });
+      </div>
+    `;
+  }
   document.getElementsByClassName("list-cards")[0].innerHTML = html;
 };
-
