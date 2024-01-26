45ba9c1351b39323624011b0bb550c05b0d04996
Victor bonnin palou <victorbonninpalou@MacBook-Pro-de-Victor.local>
2024-01-26 12:28:11 +0100

    creacion del modal para la api de geolocalizacion

--- tienda/src/js/main.js
+++ tienda/src/js/main.js
@@ -3,3 +3,14 @@ import * as bootstrap from "../bootstrap-5.3.2/js/index.umd";
 import "../bootstrap-5.3.2/scss/bootstrap.scss";
 
 import { getPopularMovies, renderPopularMovies } from "./apimovie";
+
+import {
+  initMap,
+  saveLocation,
+  loadLastLocations,
+  showLastLocations,
+  handleLocationError,
+  geocodeLatLng,
+  openModal,
+  closeModal,
+} from "./apigeolocation";
