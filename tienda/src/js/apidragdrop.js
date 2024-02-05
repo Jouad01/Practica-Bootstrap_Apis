88fe7fecc9c9619e36b5b08f87a7dfd35cf7b2a6
Victor bonnin palou <victorbonninpalou@MacBook-Pro-de-Victor.local>
2024-02-05 19:25:15 +0100

    drag and drop version completa

--- tienda/src/js/apidragdrop.js
+++ tienda/src/js/apidragdrop.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', function () {
   const openModalButtons = document.querySelectorAll('.openModalButton');
   const puzzleModal = document.getElementById('puzzleModal');
@@ -68,3 +69,44 @@ document.addEventListener('DOMContentLoaded', function () {
     puzzleModal.style.display = 'none';
   });
 });
+=======
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+let currentTrailerUrl = "";
+
+document.querySelectorAll(".trailer-button").forEach((button) => {
+  button.addEventListener("click", function () {
+    currentTrailerUrl = this.getAttribute("data-trailer-url");
+  });
+});
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+
+  $("#miModal").modal("hide");
+
+  $("#miModal").on("hidden.bs.modal", function (e) {
+    if (currentTrailerUrl) {
+      window.open(currentTrailerUrl, "_blank");
+      currentTrailerUrl = "";
+    }
+    resetDragAndDrop();
+  });
+}
+
+function resetDragAndDrop() {
+  var dragItem = document.getElementById("drag4");
+  var originalContainer = document.getElementById("images");
+  if (dragItem && originalContainer) {
+    originalContainer.appendChild(dragItem);
+  }
+}
+>>>>>>> d0cc269 (drag and drop version completa)
